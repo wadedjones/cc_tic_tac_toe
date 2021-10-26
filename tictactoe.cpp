2858e500ad5e4b61730626b19d5d3a499c98246f
Wade Jones <wadedjones@gmail.com>
2021-10-25 19:01:19 -0700

    fixed more bugs

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -58,7 +58,7 @@ void player_turn() {
 
 bool game_over() {
     for (int i=0; i<3; i++)
-    if ((board[i][0] == board[i][1] && board[i][0] == board[i][2]) || (board[0][i] == board[1][i] && board[0][1] == board[2][i]))
+    if ((board[i][0] == board[i][1] && board[i][0] == board[i][2]) || (board[0][i] == board[1][i] && board[0][i] == board[2][i]))
         return false;
     if ((board[0][0] == board[1][1] && board[0][0] == board[2][2]) || (board[2][0] == board[1][1] && board[2][0] == board[0][2]))
         return false;

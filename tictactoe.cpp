8c564edb5fc50d2a542715ecc337783b265c06a2
Wade Jones <wadedjones@gmail.com>
2021-10-25 18:44:16 -0700

    adding game over function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -55,6 +55,20 @@ void player_turn() {
     display_board();
 }
 
+bool game_over() {
+    for (int i=0; i<3; i++)
+    if ((board[i][0] == board[i][1] && board[i][0] == board[i][2]) || (board[0][i] == board[1][i] && board[0][1] == board[2][i]))
+        return false;
+    if ((board[0][0] == board[1][1] && board[0][0] == board[2][2]) || (board[2][0] == board[1][1] && board[2][0] == board[0][2]))
+        return false;
+    for (int i=0; i<3; i++)
+    for (int j=0; j<3; j++)
+    if (board[i][j] != 'X' && board[i][j] != '0')
+        return true;
+    draw = true;
+    return false;
+}
+
 
 int main() {
 

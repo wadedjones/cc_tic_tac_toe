266e114d9d6970e88dd34a2ead889e67e5147952
Wade Jones <wadedjones@gmail.com>
2021-10-25 18:09:52 -0700

    added player turn function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -21,8 +21,43 @@ void display_board() {
     cout << "     |     |     \n";
 }
 
+void player_turn() {
+    if (turn == 'X') {
+        cout << "Player One (X): \n";
+    } else if (turn == 'O') {
+        cout << "Player Two (O): \n";
+    }
+    
+    cin >> choice;
+
+    switch(choice) {
+        case 1: row=0; column=0; break;
+        case 2: row=0; column=1; break;
+        case 3: row=0; column=2; break;
+        case 4: row=1; column=0; break;
+        case 5: row=1; column=1; break;
+        case 6: row=1; column=2; break;
+        case 7: row=2; column=0; break;
+        case 8: row=2; column=1; break;
+        case 9: row=2; column=2; break;
+        default: cout << "That ain't it chief!";
+    }
+    if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O') {
+        board[row][column] = 'X';
+        turn = 'O';
+    } else if (turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O') {
+        board[row][column] = 'O';
+        turn = 'X';
+    } else {
+        cout << "IT HAS BEEN FILLED, FOOL";
+        player_turn();
+        }
+    display_board();
+}
+
 
 int main() {
 
     display_board();
+    player_turn();
 }
